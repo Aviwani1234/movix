1bc9736c0eb11597bbe5c6cd9ffe59d35b75fe48
Aviwani1234 <viratwani22@gmail.com>
2023-07-18 21:55:03 +0530

    fourth commit

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import "./style.scss"
 
 import HeroBanner from './heroBanner/HeroBanner'
+import Trending from './trending/Trending';
 
 const Home = () => {
   return (
     <div className='homePage'>
         <HeroBanner />
-        {/* <div style={{height: 1000}}></div> */}
+        <Trending />
+        <div style={{height: 1000}}></div>
     </div>
   )
 }
---
+++ src/pages/home/trending/Trending.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react'
+
+import ContentWrapper from '../../../components/contentWrapper/ContentWrapper';
+import SwitchTabs from '../../../components/SwitchTabs/SwitchTabs';
+import useFetch from '../../../hooks/useFetch';
+
+const Trending = () => {
+
+  const [endpoint, setEndpoint] = useState("day");
+
+  const {data, loading} = useFetch(`/trending/movie/${endpoint}`);
+
+  const onTabChange =(tab)=>{
+    setEndpoint(tab === "Day" ? "day" : "week");
+  };
+
+  return (
+    <div className='carouselSection'>
+        <ContentWrapper>
+                <span className='carouselTitle'>Trending</span>
+                <SwitchTabs data={["Day","Week"]} onTabChange={onTabChange}/>
+        </ContentWrapper>
+    </div>
+  );
+};
+
+export default Trending
