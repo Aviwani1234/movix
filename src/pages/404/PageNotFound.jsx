bff537aec8be87d649e46eec2f12a71c0e6dadca
Avinash Wani <95366146+Aviwani1234@users.noreply.github.com>
2023-07-24 18:33:49 +0530

    Update PageNotFound.jsx

--- src/pages/404/PageNotFound.jsx
+++ src/pages/404/PageNotFound.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import "./style.scss";
 
-import ContentWrapper from "../../components/contentWrapper/ContentWrapper";
+import ContentWrapper from "../../components/ContentWrapper/ContentWrapper";
 
 const PageNotFound = () => {
     return (
