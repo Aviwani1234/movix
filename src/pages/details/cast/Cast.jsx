7a1212ddb4c1c877a207db9b3050bd6c36a556dd
Avinash Wani <95366146+Aviwani1234@users.noreply.github.com>
2023-07-24 18:58:59 +0530

    Update Cast.jsx

--- src/pages/details/cast/Cast.jsx
+++ src/pages/details/cast/Cast.jsx
@@ -3,7 +3,7 @@ import { useSelector } from "react-redux";
 
 import "./style.scss";
 
-import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
+import ContentWrapper from "../../../components/ContentWrapper/ContentWrapper";
 import Img from "../../../components/lazyLoadImage/Img";
 import avatar from "../../../assets/avatar.png";
 
