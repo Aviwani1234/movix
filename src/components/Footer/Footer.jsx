9f940c240e8fd493d1b52ff3bebc803d56179dcd
Avinash Wani <95366146+Aviwani1234@users.noreply.github.com>
2023-07-24 18:30:10 +0530

    Update Footer.jsx

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,7 +6,7 @@ import {
     FaLinkedin,
 } from "react-icons/fa";
 
-import ContentWrapper from "../contentWrapper/ContentWrapper";
+import ContentWrapper from "../ContentWrapper/ContentWrapper";
 
 import "./style.scss";
 
